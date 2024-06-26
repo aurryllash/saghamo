2339188fa68b0d85d338fd3fcc02ab02ab5f50fa
aurryllash <lashatsiklauri77@gmail.com>
2024-06-26 19:43:12 +0400

    Add user delete functionality by admin

--- src/Routes/login.js
+++ src/Routes/login.js
@@ -25,7 +25,7 @@ router.post('/', userNotLoggedIn, async (req, res) => {
             return res.status(401).json('email or password is wrong.')
         }
 
-        const token = jwt.sign({ userId: user._id, userRole: user.role }, SECRET, { expiresIn: jwtExpireSeconds });
+        const token = jwt.sign({ userId: user._id, userRole: user.role }, SECRET, { expiresIn: jwtExpireSeconds*60000 });
         res.cookie(
             "token", token, {
             httpOnly: true,
--- src/Routes/users.js
+++ src/Routes/users.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router()
 const { User } = require('../Modules/users');
 const { requirePermits } = require('../middleware/RoleSecurity');
+const { findByIdAndDelete } = require('../Modules/products');
 
 router.get('/', requirePermits('see_users'), (req, res) => {
     User.find()
@@ -10,9 +11,21 @@ router.get('/', requirePermits('see_users'), (req, res) => {
             res.render('users', { users })
         })
         .catch(err => {
-            console.error(err); // Log the error
-            res.status(500).send('Server Error'); // Send a server error response
+            console.error(err);
+            res.status(500).send('Server Error');
         });
+})
+router.delete('/delete/:id', async (req, res) => {
+    console.log(req.params.id)
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(user) {
+            return res.status(200).json('deleted successfully')
+        }
+    } catch(error) {
+        console.log(error)
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router
