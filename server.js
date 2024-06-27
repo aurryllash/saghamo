cc43c68754fd56a4eda9cbefdb28f8a0ed5abb38
aurryllash <lashatsiklauri77@gmail.com>
2024-06-27 16:21:03 +0400

    Add 404 page for invalid routes

--- server.js
+++ server.js
@@ -46,6 +46,12 @@ app.get('/logout', (req, res) => {
     res.clearCookie('token')
     res.redirect('/home')
 })
+app.get('/404', (req, res) => {
+    res.render('404')
+}) 
+app.use((req, res, next) => {
+    res.status(404).redirect('/404');
+})
 
 
 
